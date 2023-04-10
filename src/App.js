8a89ac7e87133f9d2f71ba93200ad1df2a4199a7
Purullator <118681023+Purullator@users.noreply.github.com>
2023-04-10 17:04:16 +0200

    MONpascua

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   
@@ -27,12 +28,13 @@ const expenses = [
 ];
 
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+          <NewExpense />
+        
 
           <Expenses items={expenses}/>
         <a
