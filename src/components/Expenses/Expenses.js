af3ad30436c3ee98fdd0cef5bb9456cc91c278d4
Purullator <118681023+Purullator@users.noreply.github.com>
2023-04-19 13:26:05 +0200

    19/04section5

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,13 +14,23 @@ function Expenses(props){
         setFilteredYear(selectedYear)
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expenseContent = <p>No expenses found.</p>;
+    if(filteredExpenses.length > 0){
+        expenseContent = filteredExpenses.map(expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)
+    }
+
     return (
     
     <div>
         <Card className="expenses"> 
 
             <ExpensesFilter selectedvalue={filteredYear} onChangeFilter={filterChangeHandler} />
-            {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
+
+            {expenseContent}
           
         </Card>
     </div>
